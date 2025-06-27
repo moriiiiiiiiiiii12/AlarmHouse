d261e25f0b00b7637be952e135b4977070d6959c
moriiiiiiiiiiii12 <150291374+moriiiiiiiiiiii12@users.noreply.github.com>
2025-06-27 15:27:11 +0300

    update

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -6,36 +6,58 @@ public class Alarm : MonoBehaviour
     [SerializeField] AudioSource _audioSource;
     [SerializeField] AudioClip _audioClip;
 
-    [SerializeField] private float _minVolume = 0f;
-    [SerializeField] private float _maxVolume = 1f;
-    [SerializeField] private float _stepVolume = 0.1f; 
-    private float _currentVolume = 0f;
+    [SerializeField] private float _currentVolume = 0f;
+    [Range(0f, 1f)] [SerializeField] private float _minVolume = 0f;
+    [Range(0f, 1f)] [SerializeField] private float _maxVolume = 1f;
+    [SerializeField] private float _stepVolume = 0.1f;
 
-    private void Start() => _audioSource.clip = _audioClip;
-    
+    private Coroutine _volumeRoutine;
+
+    private void Awake()
+    {
+        _audioSource.clip = _audioClip;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.TryGetComponent(out Scammer scammer) == false) return;
+
+        _audioSource.volume = _minVolume;
+
+        if (_volumeRoutine != null)
+            StopCoroutine(_volumeRoutine);
+
         _audioSource.Play();
-        StartCoroutine(IncreaseVolume());
+        _volumeRoutine = StartCoroutine(FadeVolume(_maxVolume));
     }
 
     private void OnTriggerExit(Collider other)
     {
-        StopCoroutine(IncreaseVolume()); 
-        _audioSource.Stop();
+        if (other.TryGetComponent(out Scammer scammer) == false) return;
 
-        _currentVolume = 0f;
-        _audioSource.volume = _currentVolume; 
+        if (_volumeRoutine != null)
+            StopCoroutine(_volumeRoutine);
+
+        _volumeRoutine = StartCoroutine(FadeOutAndStop());
     }
 
-    private IEnumerator IncreaseVolume()
+    private IEnumerator FadeVolume(float targetVolume)
     {
-        while (_currentVolume < _maxVolume)
+        while (Mathf.Abs(_currentVolume - targetVolume) > 0.01f)
         {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, _maxVolume, _stepVolume * Time.deltaTime);
+            _currentVolume = Mathf.MoveTowards(_currentVolume, targetVolume, _stepVolume * Time.deltaTime);
             _audioSource.volume = _currentVolume;
 
-            yield return null; 
+            yield return null;
         }
+
+        _currentVolume = targetVolume;
+        _audioSource.volume = _currentVolume;
+    }
+    
+    private IEnumerator FadeOutAndStop()
+    {
+        yield return FadeVolume(_minVolume);    
+        _audioSource.Stop();                    
     }
 }
---
+++ Assets/Scripts/Scammer.cs
@@ -0,0 +1,7 @@
+
+using UnityEngine;
+
+class Scammer : MonoBehaviour
+{
+    
+}
